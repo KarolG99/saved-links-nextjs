6625b4a3c70838ad5400d4ead003fad8bd3d1115
KarolG99 <karolus2212@gmail.com>
2023-01-14 10:10:01 +0100

    starter point

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,18 @@
-import './globals.css'
+import { Baloo_2 } from "@next/font/google";
+
+import "./globals.css";
+
+const baloo2 = Baloo_2({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
+    <html lang="en" className={baloo2.className}>
       <head />
       <body>{children}</body>
     </html>
-  )
+  );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}", // Note the addition of the `app` directory.
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        bgColor: "#faf8ff",
+        primaryBlue: "#0066ff",
+        darkText: "#001085",
+      },
+    },
+  },
+  plugins: [],
+};
