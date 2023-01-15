c63b36db4a81a1517342657cdd118c2131eaa5b7
KarolG99 <karolus2212@gmail.com>
2023-01-15 11:15:30 +0100

    Fix url to get links

--- lib/getUserLinks.ts
+++ lib/getUserLinks.ts
@@ -1,4 +1,4 @@
 export default async function getUserLinks(id: string) {
-  const userLinks = await fetch(`http://localhost:3000/api/get-links/${id}`);
+  const userLinks = await fetch(`/api/get-links/${id}`);
   return userLinks.json();
 }
